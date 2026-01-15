5735539c2aebdb50bbb89a6215b1320b038b0701
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2026-01-15 20:23:38 +0500

    add app.py model.ipynb model.pkl and update README.md

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st 
+import pickle
+
+st.set_page_config(page_title="Income Predictor", page_icon="💰")
+
+st.title("Income Predictor")
+st.caption("This project is built for the AI15 and DS14")
+
+with open("model.pkl", "rb") as file:
+    model = pickle.load(file)
+
+
+input = st.number_input(label="Enter years of Experience", min_value=0.0, max_value=10.0, value=1.5, step=0.1)
+
+if st.button("Predict"):
+    predictions = model.predict([[input]])[0]
+    st.success(predictions)
